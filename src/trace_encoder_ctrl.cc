bd004cea7c202c3ec5984974d802c8af8a5c3ced
Lux <iansseijelly@berkeley.edu>
2025-10-03 11:50:33 -0700

    refactor: Comment out unused context mode and ASID settings, update compatible string in FDT parsing

--- src/trace_encoder_ctrl.cc
+++ src/trace_encoder_ctrl.cc
@@ -28,15 +28,15 @@ bool trace_encoder_ctrl_t::store(reg_t addr, size_t len, const uint8_t* bytes) {
             printf("[TRACE_ENCODER_CTRL]: Setting br_mode to %d\n", bytes[0]);
             this->encoder->set_br_mode(static_cast<br_mode_t>(bytes[0]));
             return true;
-        case TR_TE_CTX_MODE:
-            printf("[TRACE_ENCODER_CTRL]: Setting ctx_mode to %d\n", bytes[0]);
-            this->encoder->set_ctx_mode(static_cast<ctx_mode_t>(bytes[0]));
-            return true;
-        case TR_TE_CTX_ASID:
-            uint32_t ctx_id = bytes[3] << 24 | bytes[2] << 16 | bytes[1] << 8 | bytes[0];
-            printf("[TRACE_ENCODER_CTRL]: Setting ctx_id to %d\n", ctx_id);
-            this->encoder->set_ctx_id(ctx_id);
-            return true;
+        // case TR_TE_CTX_MODE:
+        //     printf("[TRACE_ENCODER_CTRL]: Setting ctx_mode to %d\n", bytes[0]);
+        //     this->encoder->set_ctx_mode(static_cast<ctx_mode_t>(bytes[0]));
+        //     return true;
+        // case TR_TE_CTX_ASID:
+        //     uint32_t ctx_id = bytes[3] << 24 | bytes[2] << 16 | bytes[1] << 8 | bytes[0];
+        //     printf("[TRACE_ENCODER_CTRL]: Setting ctx_id to %d\n", ctx_id);
+        //     this->encoder->set_ctx_id(ctx_id);
+        //     return true;
         default:
             return false;
     }
@@ -61,7 +61,7 @@ int trace_encoder_ctrl_parseFDT(const void *fdt, reg_t *address,
 // This function parses an instance of this device from the FDT
 // An FDT node for a device should encode, at minimum, the base address for the device
 trace_encoder_ctrl_t* trace_encoder_ctrl_parseFromFDT(const void* fdt, const sim_t* sim, reg_t* base, std::vector<std::string> sargs) {
-  if (trace_encoder_ctrl_parseFDT(fdt, base, "ucbbar,trace_encoder_ctrl") == 0) {
+  if (trace_encoder_ctrl_parseFDT(fdt, base, "ucbbar,trace") == 0) {
     printf("Found trace_encoder_ctrl at 0x%lx\n", *base);
     return new trace_encoder_ctrl_t((const_cast<sim_t*>(sim)->get_core(0)->trace_encoder));
   } else {
@@ -71,17 +71,15 @@ trace_encoder_ctrl_t* trace_encoder_ctrl_parseFromFDT(const void* fdt, const sim
 
 std::string trace_encoder_ctrl_generateDTS(const sim_t* sim, const std::vector<std::string>& args) {
   std::stringstream s;
-  s << std::hex
-    << "    trace_encoder_ctrl: trace_encoder_ctrl@" << TRACE_ENCODER_CTRL_BASE << " {\n"
-       "      compatible = \"ucbbar,trace_encoder_ctrl\";\n"
-       "      interrupt-parent = <&PLIC>;\n"
-       "      interrupts = <" << std::dec << 2;
-  reg_t blkdevbs = TRACE_ENCODER_CTRL_BASE;
-  reg_t blkdevsz = TRACE_ENCODER_CTRL_SIZE;
-  s << std::hex << ">;\n"
-       "      reg = <0x" << (blkdevbs >> 32) << " 0x" << (blkdevbs & (uint32_t)-1) <<
-                   " 0x" << (blkdevsz >> 32) << " 0x" << (blkdevsz & (uint32_t)-1) << ">;\n"
-       "    };\n";
+  reg_t base = TRACE_ENCODER_CTRL_BASE;
+  reg_t size = TRACE_ENCODER_CTRL_SIZE;
+  
+  s << "    trace_encoder_ctrl: trace_encoder_ctrl@" << std::hex << base << " {\n"
+    << "      compatible = \"ucbbar,trace\";\n"
+    << "      reg = <0x" << (base >> 32) << " 0x" << (base & 0xffffffff)
+    << " 0x" << (size >> 32) << " 0x" << (size & 0xffffffff) << ">;\n"
+    << "    };\n";
+  
   return s.str();
 }
 
